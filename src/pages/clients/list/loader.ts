7cda3b7f39024b549a19bea5eaf7bbb36d7b8ab3
jFroylan-Cal <j.froylan_calderon@outlook.com>
2023-06-24 18:40:47 -0600

    [Clients-List]: Implementation of new logic using loader

---
+++ src/pages/clients/list/loader.ts
@@ -0,0 +1,24 @@
+import { ApiRequestProviderInstance } from '~utils/ApiRequestProvider';
+import { ApiMethods } from '~types/api/api-methods-object.type';
+import { IClientsListApiResponse } from '~types/clients/clients-list-object';
+import { defer } from 'react-router-dom';
+
+export const listClientLoader = async () => {
+  const url = '/clientes';
+  const apiRequestProvider = ApiRequestProviderInstance;
+
+  const apiResponse = await apiRequestProvider.doRequest({
+    url,
+    method: ApiMethods.GET,
+    requireAuth: true,
+  });
+
+  const apiResponseData: Array<IClientsListApiResponse> = await apiResponse.json();
+  
+  const results = apiResponseData.map((client) => ({
+    id: client.id,
+    name: client.nombre,
+    slug: client.slug,
+  }));
+  return defer({ results });
+};
--- src/pages/clients/list/routes.tsx
+++ src/pages/clients/list/routes.tsx
@@ -2,6 +2,7 @@ import { NonIndexRouteObjectWithNav } from '~types/router/route-object-with-nav.
 import { ClientsListElement } from './clients-list.element';
 import { RouteURLs } from './urls';
 import { ClientsListErrorElement } from './clients-list-error.element';
+import { listClientLoader } from './loader';
 
 export const routes: Array<NonIndexRouteObjectWithNav> = [
   {
@@ -10,6 +11,7 @@ export const routes: Array<NonIndexRouteObjectWithNav> = [
     errorElement: <ClientsListErrorElement />,
     name: RouteURLs.ROOT,
     showInNav: false,
+    loader: listClientLoader,
     order: 1,
   },
 ];

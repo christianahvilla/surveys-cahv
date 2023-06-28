89609e1c2feee47408bf095deaed5f181850341a
christianahvilla <christianahvilla@gmail.com>
2023-06-28 10:30:30 -0600

    [Users-Edit]: Add component to edit users

---
+++ src/pages/users/edit/routes.tsx
@@ -0,0 +1,17 @@
+import { NonIndexRouteObjectWithNav } from '~types/router/route-object-with-nav.type';
+import { RouteURLs } from './urls';
+import { UsersEditElement } from './users-edit.element';
+import { editUserAction } from './edit-action';
+import { getUserLoader } from './loader';
+
+export const routes: Array<NonIndexRouteObjectWithNav> = [
+  {
+    path: RouteURLs.ROOT,
+    element: <UsersEditElement />,
+    name: RouteURLs.ROOT,
+    action: editUserAction,
+    loader: getUserLoader,
+    showInNav: false,
+    order: 1,
+  },
+];

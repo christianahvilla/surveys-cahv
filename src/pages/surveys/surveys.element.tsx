16b36e405abb4a2498415f6eb876109e2ab2c08d
jFroylan-Cal <j.froylan_calderon@outlook.com>
2023-06-30 14:34:50 -0600

    [surveys:create] Add all files for create a survey

--- src/pages/surveys/surveys.element.tsx
+++ src/pages/surveys/surveys.element.tsx
@@ -1,11 +1,5 @@
+import { Outlet } from 'react-router-dom';
+
 export const SurveysElement = () => {
-  return (
-    <div data-testid='surveys-element'>
-      <div className='min-h-screen w-full bg-gray-50 px-40 pt-12 pb-12 pl-80' id='content'>
-        <h3 className='my-6 mt-0 text-[1.75rem] font-medium leading-[1.2] flex justify-self-start text-gray-500'>
-          Surveys
-        </h3>
-      </div>
-    </div>
-  );
+  return <Outlet />;
 };

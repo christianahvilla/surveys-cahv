c77a83f1c5d8045f058623e91683e57b6d37a2e2
christianahvilla <christianahvilla@gmail.com>
2023-07-31 18:13:03 -0600

    [FIX]: Create survey is working

--- src/pages/surveys/edit/loader.ts
+++ src/pages/surveys/edit/loader.ts
@@ -1,30 +1,51 @@
 import { ApiRequestProviderInstance } from '~utils/ApiRequestProvider';
 import { ApiMethods } from '~types/api/api-methods-object.type';
-import { ISurveyListApiResponse } from '~types/surveys/surveys-list-object';
 import { defer } from 'react-router-dom';
+import { ISurveyApiResponse } from '~types/surveys/surveys-list-object';
+import { IClientsListApiResponse } from '~types/clients/clients-list-object';
 
 export const getSurveysLoader = async ({ params }: any) => {
-  const url = `/encuestas/${params.id}`;
+  const urlSurvey = `/encuestas/filter/${params.id}`;
+  const urlClient = `/clientes`;
 
   const apiRequestProvider = ApiRequestProviderInstance;
 
-  const apiResponse = await apiRequestProvider.doRequest({
-    url,
+  const apiResponseSurvey = await apiRequestProvider.doRequest({
+    url: urlSurvey,
     method: ApiMethods.GET,
     requireAuth: true,
   });
 
-  const apiResponseData: ISurveyListApiResponse = await apiResponse.json();
+  const apiResponseClient = await apiRequestProvider.doRequest({
+    url: urlClient,
+    method: ApiMethods.GET,
+    requireAuth: true,
+  });
+
+  const apiResponseDataSurvey: ISurveyApiResponse = await apiResponseSurvey.json();
+  const apiResponseDataClients: IClientsListApiResponse = await apiResponseClient.json();
+
+  const clients = apiResponseDataClients.map((client) => ({
+    id: client.id,
+    name: client.nombre,
+    slug: client.slug,
+  }));
+
+  const survey = {
+    id: apiResponseDataSurvey.id,
+    name: apiResponseDataSurvey.nombre,
+    slug: apiResponseDataSurvey.slug,
+    description: apiResponseDataSurvey.descripcion,
+    client: {
+      id: apiResponseDataSurvey.cliente.id,
+      name: apiResponseDataSurvey.cliente.nombre,
+    },
+  };
 
   const results = {
-    id: apiResponseData.id,
-    name: apiResponseData.nombre,
-    slug: apiResponseData.slug,
-    description: apiResponseData.descripcion,
-    client: apiResponseData.cliente,
+    survey,
+    clients,
   };
 
   return defer({ results });
 };
-
-
